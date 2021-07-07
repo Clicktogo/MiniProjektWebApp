d743371050f74d452cce31a00702f5adcf9bd7a5
Filip Garcia <mailafilip@gmail.com>
2021-07-07 16:58:46 +0200

    added attribute to concert

--- src/main/java/com/example/demo/Concert.java
+++ src/main/java/com/example/demo/Concert.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.Date;
 
 public class Concert implements Event{
+    private int concertId;
     private String artist;
     private String date;
     private String address;
