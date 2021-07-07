77fee1b69e8f10ae9c9e3748e844d1634c56e273
Filip Garcia <mailafilip@gmail.com>
2021-07-07 16:21:57 +0200

    new class Concert and interface Event

---
+++ src/main/java/com/example/demo/Concert.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class Concert implements Event{
+    private String artist;
+    private String date;
+    private int ticketPrice;
+    private String location;
+
+}
