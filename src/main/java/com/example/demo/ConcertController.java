cfd8a3958489e324f129ddfc24ce898cc426823d
Daniel <daniel.gidlund@outlook.com>
2021-07-08 14:35:59 +0200

    addSortingList

--- src/main/java/com/example/demo/ConcertController.java
+++ src/main/java/com/example/demo/ConcertController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -20,11 +21,23 @@ public class ConcertController {
         return "allEvents";
     }
 
-    @GetMapping("/events")
+    @GetMapping("/event")
     public String detailPage(Model model, @RequestParam int eventId)    {
         model.addAttribute("currentConcert", service.getConcertById(eventId));
 
         return "masterDetailConcert";
+    }
+
+    @PostMapping("/")
+    public String filter(Model model, @RequestParam String filterSelection) {
+        if (filterSelection.equalsIgnoreCase("Pris"))   {
+            model.addAttribute("concertList", service.sortByPrice());
+        }
+        else if (filterSelection.equalsIgnoreCase("Artist"))    {
+            service.sortByArtistName();
+            model.addAttribute("concertList", service.sortByArtistName());
+        }
+        return "allEvents";
     }
 
 }
