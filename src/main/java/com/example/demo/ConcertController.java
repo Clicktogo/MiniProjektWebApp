85eb60de82841e70c90ee255fc6cf8131b1c81be
Daniel <daniel.gidlund@outlook.com>
2021-07-08 10:32:32 +0200

    cards created

--- src/main/java/com/example/demo/ConcertController.java
+++ src/main/java/com/example/demo/ConcertController.java
@@ -2,11 +2,21 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class ConcertController {
 
     @Autowired
     EventService service;
+
+    @GetMapping("/")
+    public String allConcerts(Model model)  {
+        model.addAttribute("concertList", service.getAllConcerts());
+        return "allEvents";
+    }
 
 }
