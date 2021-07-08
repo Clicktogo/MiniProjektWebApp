b340d6e0cd70e339a89db18a79560f253c0c5a87
Daniel <daniel.gidlund@outlook.com>
2021-07-08 11:55:17 +0200

    detaljsida

--- src/main/java/com/example/demo/ConcertController.java
+++ src/main/java/com/example/demo/ConcertController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -17,6 +18,13 @@ public class ConcertController {
     public String allConcerts(Model model)  {
         model.addAttribute("concertList", service.getAllConcerts());
         return "allEvents";
+    }
+
+    @GetMapping("/events")
+    public String detailPage(Model model, @RequestParam int eventId)    {
+        model.addAttribute("currentConcert", service.getConcertById(eventId));
+
+        return "masterDetailConcert";
     }
 
 }
